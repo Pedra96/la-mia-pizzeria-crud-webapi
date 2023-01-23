3e49fb00657462aa2dd48761e71e0d96da719a79
Pedra96 <darkraidialga@yahoo.it>
2023-01-23 15:33:15 +0100

    Bonus completato

--- La Mia Pizzeria 1/wwwroot/js/site.js
+++ La Mia Pizzeria 1/wwwroot/js/site.js
@@ -1,4 +1,41 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function loadList() {
 
-// Write your JavaScript code.
+    axios.get('/api/PizzaApi').then((res) => {
+
+        console.log('risultato ok', res);
+
+        if (res.data.length == 0) {
+
+            document.querySelector('.js_no_post').classList.remove('d-none');
+            document.querySelector('.js_post_table').classList.add('d-none');
+
+        } else {
+
+            document.querySelector('.js_post_table').classList.remove('d-none');
+            document.querySelector('.js_no_post').classList.add('d-none');
+
+            document.querySelector('.js_post_table').innerHTML = '';
+
+            res.data.forEach(post => {
+
+                console.log('post', post);
+
+                document.querySelector('.js_post_table').innerHTML +=
+                    `
+                                   <div class="col-12 col-md-4 p-2">
+                                    <div class="card post h-100">
+                                      <img src="${post.image}" class="card-img-top" alt="...">
+                                      <div class="card-body">
+                                        <h5 class="card-title">${post.title}</h5>
+                                        <p class="card-text">${post.description}</p>
+                                      </div>
+                                    </div>
+                                </div>
+                             `;
+            });
+        }
+    }).catch((error) => {
+        console.log(error);
+    });
+
+}
