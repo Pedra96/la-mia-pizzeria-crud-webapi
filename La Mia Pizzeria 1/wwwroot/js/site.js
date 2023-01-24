9d5f094364805f8934c82e2700beea090d616da8
Pedra96 <darkraidialga@yahoo.it>
2023-01-24 14:34:15 +0100

    finita funzione di ricerca

--- La Mia Pizzeria 1/wwwroot/js/site.js
+++ La Mia Pizzeria 1/wwwroot/js/site.js
@@ -1,10 +1,10 @@
 ﻿function searchItem() {
+    console.log("enter");
     let searchValue = document.getElementById("SearchBar").value;
     loadList(searchValue);
 }
 
-function loadList(searchString) {
-
+function init() {
     axios.get('/api/PizzaApi').then((res) => {
 
         console.log('risultato ok', res);
@@ -18,6 +18,54 @@ function loadList(searchString) {
 
             document.getElementById("js_post_table").classList.remove('d-none');
             document.getElementById("js_no_post").classList.add('d-none');
+
+            document.getElementById("js_post_table").innerHTML = '';
+
+            res.data.forEach(pizza => {
+
+                console.log('pizza', pizza);
+
+                document.getElementById("js_post_table").innerHTML +=
+                    `
+                                   <div class="col-12 col-md-4 p-2">
+                                    <div class="card post h-100">
+                                        
+                                      <img src="${pizza.image}" class="card-img-top" alt="...">
+                                      <div class="card-body">
+                                        <h5 class="card-title">${pizza.title}</h5>
+                                         <a href="/Pizze/Details/${pizza.id}"
+                                        <p class="card-text">${pizza.description}</p>
+                                      </div>
+                                      </a>
+                                    </div>
+                                </div>
+                             `;
+            });
+        }
+    }).catch((error) => {
+        console.log(error);
+    });
+}
+
+function loadList(searchString) {
+
+    axios.get('/api/PizzaApi', {
+        params: {
+            search: searchString
+            }
+        }).then((res) => {
+
+        console.log('risultato ok', res);
+
+        if (res.data.length == 0) {
+
+            document.getElementById("js_no_found").classList.remove('d-none');
+            document.getElementById("js_post_table").classList.add('d-none');
+
+        } else {
+
+            document.getElementById("js_post_table").classList.remove('d-none');
+            document.getElementById("js_no_found").classList.add('d-none');
 
             document.getElementById("js_post_table").innerHTML = '';
 
